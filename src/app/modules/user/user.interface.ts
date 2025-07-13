364b3551fd232df8bae577caca7a10d5da235759
Jeem312 <shanjidajeem312@gmail.com>
2025-07-13 21:05:55 +0600

    Add HTTP status code libraries to project dependencies
    Integrates http-status and http-status-codes libraries into the project, providing standardized HTTP status code constants and utilities. Includes package metadata, license files, and documentation to support usage. Enhances code maintainability and clarity by introducing pre-defined status codes for HTTP responses.

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,36 @@
+import { Types } from "mongoose";
+
+export enum Role {
+    SUPER_ADMIN = 'SUPER_ADMIN',
+    ADMIN = 'ADMIN',
+    USER = 'USER',
+    GUIDE = 'GUIDE',
+}
+export enum isActive {
+    ACTIVE = 'ACTIVE',
+    INACTIVE = 'INACTIVE',
+    BLOCKED = 'BLOCKED',
+}
+
+export interface IAuthProvider {
+
+    provider: string;
+    providerId: string;}
+
+export interface IUser {
+
+    email: string;
+    name: string;
+    role: Role;
+    password?: string;
+    phone?: string;
+    picture?: string;
+    address?: string;
+    isDeleted?: string;
+    isActive?: isActive;
+    isVerified?: string;
+    auths?: IAuthProvider[];
+    bookings?: Types.ObjectId[];
+    guides?: Types.ObjectId[];
+
+}
---
+++ src/app/modules/user/user.models.ts
@@ -0,0 +1,28 @@
+import { model, Schema } from "mongoose";
+import { IAuthProvider, isActive, IUser, Role } from "./user.interface";
+const authProviderSchema = new Schema<IAuthProvider>({
+    provider: { type: String, required: true },
+    providerId: { type: String, required: true }
+});
+const userSchema = new Schema<IUser>({
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    role: { type: String, enum: Object.values(Role), default: Role.USER },
+    password: { type: String },
+    phone: { type: String },
+    picture: { type: String },
+    address: { type: String },
+    isDeleted: { type: Boolean, default: false },
+    isActive: { 
+        type: String, 
+        enum: Object.values(isActive), 
+        default: isActive.ACTIVE },
+    isVerified: { type: Boolean, default: false },
+    auths: [authProviderSchema],
+ 
+}
+, { timestamps: true ,
+    versionKey: false
+});
+const User = model<IUser>("User", userSchema);
+export default User;
