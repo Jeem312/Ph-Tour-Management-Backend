96474750f132b561bd873800b6fd055bc3c178a3
Jeem312 <shanjidajeem312@gmail.com>
2025-07-23 18:32:16 +0600

    Enhances error handling and authentication logic
    Refines Google authentication by ensuring a password is required for users authenticated with Google to log in with credentials.
    
    Extends global error handler to cover duplicate key errors, MongoDB ObjectId validation, and Zod validation errors, improving error specificity and feedback.
    
    Revises login flow to leverage Passport.js for local authentication, improving error handling and token generation.

--- src/app/middleWares/globalErrorHandler.ts
+++ src/app/middleWares/globalErrorHandler.ts
@@ -2,16 +2,57 @@
 import { Request, Response, NextFunction } from "express";
 import { envConfig } from "../config/env"
 import AppError from "../errorHelpers/AppError";
+import { object } from "zod";
 
 export const globalErrorHandler = (err:any, req: Request, res: Response, next:NextFunction) => {
 
     let statusCode = 500;
     let message =  `Something went wrong ${err.message}`;
+     const errorSource: any = [{
+      // path:"isDeleted",
+      // message: "Cast error",
+
+  }]
     
 if(err instanceof AppError){
   statusCode = err.statusCode;
   message = err.message
   
+}
+// duplicate key error
+else if(err.code === 11000){
+  const matchedArray = err.message.match(/"([^"]*)"/);
+  statusCode = 400;
+  message = `Duplicate field value: ${matchedArray[1]}.already exists.`;
+}
+// mongoDB objectId error
+else if(err.name === "castError"){
+  statusCode = 400;
+  message = `Invalid MongoDB ID: ${err.value}`;
+}
+// validation error 
+else if(err.name === "ValidationError"){
+  statusCode = 400;
+  const errors = Object.values(err.errors);
+ 
+  errors.forEach((errorObject: any) => {
+    errorSource.push({
+      path: errorObject.path,
+      message: errorObject.message,
+    });
+  });
+}
+
+// zod validation error
+else if (err.name === "ZodError") {
+  statusCode = 400;
+  message="Zod Error";
+  err.issues.forEach((issue:any)=>{
+    errorSource.push({
+      path:issue.path[issue.path.length - 1],
+      message:issue.message
+    })
+  })
 }
 else if (err instanceof Error) {
   statusCode = 500;
@@ -22,6 +63,7 @@ else if (err instanceof Error) {
   res.status(statusCode).json({
     success: false,
     message,
+    errorSource,
     error: err,
     stack: envConfig.NODE_ENV === "development" ? err.stack : undefined
   });
