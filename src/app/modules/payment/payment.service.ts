4a115ff5b6876099e52d7a9126aa9d8c0ed657fd
Jeem312 <shanjidajeem312@gmail.com>
2025-09-08 18:31:15 +0600

    added sslCommerce service code

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -1,43 +1,53 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 
 
 // const getTransactionId = () => {
 //     return `tran_${Date.now()}_${Math.floor(Math.random() * 1000)}`
 // }
+import httpStatus from "http-status-codes";
+
+import AppError from "../../errorHelpers/AppError";
+import Booking from "../booking/booking.model";
+import { ISSLCommerz } from "../sslCommerce/sslCommerce.interface";
+import { SSLService } from "../sslCommerce/sslCommerce.service";
+import Payment from "./payment.model";
 
 
-const createPayment = async () => {
-   return{
+const initPayment = async (bookingId: string) => {
+    const payment = await Payment.findOne({ booking: bookingId })
 
-   }
+    if (!payment) {
+        throw new AppError(httpStatus.NOT_FOUND, "Payment Not Found. You have not booked this tour")
+    }
+
+    const booking = await Booking.findById(payment.booking)
+
+    const userAddress = (booking?.user as any).address
+    const userEmail = (booking?.user as any).email
+    const userPhoneNumber = (booking?.user as any).phone
+    const userName = (booking?.user as any).name
+
+    const sslPayload: ISSLCommerz = {
+        address: userAddress,
+        email: userEmail,
+        phoneNumber: userPhoneNumber,
+        name: userName,
+        amount: payment.amount,
+        transactionId: payment.transactionId
+    }
+
+    const sslPayment = await SSLService.sslPaymentInit(sslPayload)
+
+    return {
+        paymentUrl: sslPayment.GatewayPageURL
+    }
+
 };
 
 
-const getUserPayments = async () => {
 
-    return {}
-};
-
-const getPaymentById = async () => {
-    return {}
-};
-
-const updatePaymentStatus = async (
-
-) => {
-
-    return {}
-};
-
-const getAllPayments = async () => {
-
-    return {}
-};
 
 export const PaymentService = {
-    createPayment,
-    getUserPayments,
-    getPaymentById,
-    updatePaymentStatus,
-    getAllPayments,
+    initPayment
 };
---
+++ src/app/modules/payment/paymet.route.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { PaymentController } from "./payment.controller";
+
+
+const router = express.Router();
+
+
+router.post("/init-payment/:bookingId", PaymentController.initPayment);
+
+export const PaymentRoutes = router;
